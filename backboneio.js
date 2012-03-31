53d63112bf74236d120e68f05ad20f561182d1c8
Billy Vong <billyvg@gmail.com>
2012-03-30 19:21:47 -0700

    Fix event triggers for server.

--- backboneio.js
+++ backboneio.js
@@ -1,6 +1,5 @@
 (function() {
-  var Backbone, Backboneio, getValue, io, root, server, socketQueue, _, _socket, _sockets,
-    __slice = Array.prototype.slice,
+  var Backbone, Backboneio, getValue, io, root, server, _,
     __hasProp = Object.prototype.hasOwnProperty,
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
 
@@ -12,65 +11,88 @@
 
   io = root.io || (!(io != null) && (typeof require !== "undefined" && require !== null) ? require('socket.io') : void 0);
 
-  socketQueue = {};
+  if (typeof exports !== 'undefined') {
+    exports.Backboneio = Backboneio = {};
+    server = true;
+  } else {
+    root.Backboneio = Backboneio = {};
+    server = false;
+  }
 
-  socketQueue = _.extend(socketQueue, Backbone.Events, {
+  Backboneio.socketQueue = {};
+
+  Backboneio._sockets = {};
+
+  _.extend(Backboneio.socketQueue, Backbone.Events, {
     _eventQueue: {}
   });
 
-  _sockets = _socket = false;
-
-  Backboneio = {
-    connect: function(connect) {
-      if (connect != null) return this._route('_connect', connect);
-    },
-    _route: function() {
-      var method, params;
-      method = arguments[0], params = 2 <= arguments.length ? __slice.call(arguments, 1) : [];
-      method += server ? '__server' : '';
-      if (this[method] != null) return this[method].apply(this, params);
-    },
-    _connect: function(ioserver) {
-      console.log('connecting to socket');
-      if (ioserver != null) _socket = io.connect(ioserver);
-      _sockets = io.sockets;
-      return socketQueue.trigger('connect.bbio');
-    },
-    _connect__server: function(port) {
-      io = io.listen(port);
-      _sockets = io.sockets;
-      _sockets.on('connection', function(socket) {
-        console.log('connection');
-        _socket = socket;
-        return socketQueue.trigger('connect.bbio connect.server.bbio');
-      });
-      return _sockets.on('connection', function(socket) {
-        return console.log('connection2');
-      });
-    }
+  Backboneio.Socket = function(options) {
+    this.options = this.options || {};
+    options = _.extend(this.options, options);
+    return this.initialize.apply(this, arguments);
   };
+
+  _.extend(Backboneio.Socket.prototype, Backbone.Events, {
+    initialize: function(options) {
+      if (server) Backboneio.View.prototype.socket = this;
+      if (options.port != null) return this.connect();
+    },
+    connect: function() {
+      if (server) this._connect_server();
+      if (!server) this._connect();
+    },
+    _connect: function() {
+      var full_hostname, _socket, _sockets;
+      if ((this.options.host != null) && (this.options.port != null)) {
+        full_hostname = 'http://' + this.options.host + ':' + this.options.port;
+        _socket = io.connect(full_hostname);
+        _sockets = io.sockets;
+        Backboneio.View.prototype.socket = _socket;
+        return this.trigger('bbio.connect', _socket);
+      }
+    },
+    _connect_server: function() {
+      var _this = this;
+      if (this.options.port != null) {
+        io = io.listen(this.options.port);
+        this.io = io.sockets;
+        Backboneio.View.prototype.sockets = this.io;
+        return this.io.on('connection', function(socket) {
+          var $emit;
+          _this.trigger('bbio.connect', socket);
+          $emit = socket.$emit;
+          return socket.$emit = function(event, data) {
+            _this.trigger(event, socket, data);
+            return $emit.apply(socket, arguments);
+          };
+        });
+      }
+    }
+  });
 
   Backboneio.View = (function(_super) {
 
     __extends(View, _super);
 
     function View(options) {
-      socketQueue.on('connect.bbio', this._socketConnected, this);
       this.cid = _.uniqueId('view');
       this._configure(options || {});
       if (!server) this._ensureElement();
-      if ((options != null) && (options.connect != null)) {
-        Backboneio.connect(options.connect);
-      }
-      if (_sockets) this.sockets = _sockets;
-      if (_socket) this.socket = _socket;
       this.initialize.apply(this, arguments);
       this.delegateEvents();
     }
 
+    View.prototype.initialize = function() {};
+
+    View.prototype._configure = function(options) {
+      View.__super__._configure.call(this, options);
+      if (options.socket) return this.socket = options.socket;
+    };
+
     View.prototype.delegateEvents = function(events) {
       this.delegateSockets();
-      return View.__super__.delegateEvents.call(this, events);
+      if (!server) return View.__super__.delegateEvents.call(this, events);
     };
 
     View.prototype.delegateSockets = function(events) {
@@ -89,11 +111,9 @@
         eventName = key;
         this._sevents_lookup[method] = eventName;
         if (this.socket != null) {
-          console.log('socket is set', eventName);
           _results.push(this.socket.on(eventName, method));
         } else {
-          console.log('queueing: ', eventName);
-          _results.push(socketQueue._eventQueue[eventName] = method);
+          _results.push(Backboneio.socketQueue._eventQueue[eventName] = method);
         }
       }
       return _results;
@@ -103,42 +123,44 @@
       var args, channel;
       if (arguments.length === 1) {
         channel = this.channel != null ? this.channel : this._sevents_lookup[arguments.callee.caller.toString()];
-        args = arguments;
+        args = arguments[0];
       } else {
         channel = arguments[0];
         args = arguments[1];
       }
-      console.log('emit', channel, args);
       return this.socket.emit(channel, args);
     };
 
     View.prototype.undelegateSockets = function() {
-      return '';
-    };
-
-    View.prototype._socketConnected = function() {
-      if (_sockets) this.sockets = _sockets;
-      if (_socket) this.socket = _socket;
-      return this._delegateQueue();
+      var eventName, _i, _len, _ref, _results;
+      if (this.socket.removeAllListeners != null) {
+        return this.socket.removeAllListeners();
+      } else {
+        _ref = this._sevents_lookup;
+        _results = [];
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          eventName = _ref[_i];
+          _results.push(this.socket.off(eventName));
+        }
+        return _results;
+      }
     };
 
     View.prototype._delegateQueue = function() {
-      var eventName, method, _ref, _results;
-      console.log('delegateQueue called', socketQueue._eventQueue);
-      _ref = socketQueue._eventQueue;
-      _results = [];
-      for (eventName in _ref) {
-        method = _ref[eventName];
-        _results.push(this.socket.on(eventName, method));
+      var eventName, method, _ref;
+      if (this.socket != null) {
+        _ref = socketQueue._eventQueue;
+        for (eventName in _ref) {
+          method = _ref[eventName];
+          this.socket.on(eventName, method);
+        }
+        return socketQueue._eventQueue = {};
       }
-      return _results;
     };
 
     return View;
 
   })(Backbone.View);
-
-  root.socketQueue = socketQueue;
 
   getValue = function(object, prop) {
     if (!(object && object[prop])) return null;
@@ -148,13 +170,5 @@
       return object[prop];
     }
   };
-
-  if (typeof exports !== 'undefined') {
-    exports.Backboneio = Backboneio;
-    server = true;
-  } else {
-    root.Backboneio = Backboneio;
-    server = false;
-  }
 
 }).call(this);
